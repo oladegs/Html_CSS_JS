738774be152843dfbd40624851e9e0cd62b0a7f2
oladegs <adeyinkaoladega@gmail.com>
2025-04-24 09:31:21 -0400

    Delete prod from cart when delete is clicked

--- javascript-amazon-project/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project/javascript-amazon-project-main/data/cart.js
@@ -1,10 +1,10 @@
-export const cart = [
+export let cart = [
   {
-    productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
   },
   {
-    productID: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
   },
 ];
@@ -30,4 +30,21 @@ export function addToCart(productId) {
     cart.push({ productId: productId, quantity: 1 });
   }
   console.log(cart);
+}
+
+/*
+1. Create a new array 
+2. Loop through the cart 
+3. Add each product to the new array, except for this productId
+*/
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,11 +1,11 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
-  const productId = cartItem.productID;
+  const productId = cartItem.productId;
 
   let matchingProduct;
 
@@ -38,7 +38,9 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link"data-product-id="${
+                    matchingProduct.id
+                  }">
                     Delete
                   </span>
                 </div>
@@ -89,3 +91,17 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+/*
+To delete :
+1. Remove the product from the cart 
+2. Update the HTML
+*/
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+  });
+});
