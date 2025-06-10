b5a294c9fffd74a175a7e3699afae02476f7d4e1
oladegs <adeyinkaoladega@gmail.com>
2025-06-09 23:51:41 -0400

    Load products in beforeAll hook

--- JS-lesson-amazon-project-main/tests/checkout/orderSummaryTest.js
+++ JS-lesson-amazon-project-main/tests/checkout/orderSummaryTest.js
@@ -1,11 +1,19 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { cart, loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe("test suite: renderOrderSummary", () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
-
+  // We only need to load the product once for all of our test
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
   // Before each test, this chunk of code runs first
+  // Jasmine has a feature called - done function : waiting for some code to finish, lets us control when to go to the next step
+
   beforeEach(() => {
     spyOn(localStorage, "setItem");
 
